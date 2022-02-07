8cde751710f4aa7f98b13112040f6b203ed092d8
Haiss2 <hai98th@gmail.com>
2022-02-07 09:50:04 +0700

    add coin

---
+++ client/index.js
@@ -0,0 +1,10 @@
+const Web3 = require('web3');
+const Coin = require('./contracts/HaiLe.json')
+
+const init = async () => {
+    const web3 = new Web3("http://127.0.0.1:9545/")
+
+    console.log(web3)
+}
+
+init()
